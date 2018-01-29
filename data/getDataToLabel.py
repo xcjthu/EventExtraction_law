f61d48c8ba8b8d65fd51816972b030b9a5e9d01b
xiaocj <xiaocj16@mails.tsinghua.edu.cn>
2018-01-29 20:18:31 +0800

    get data to label

---
+++ data/getDataToLabel.py
@@ -0,0 +1,37 @@
+import json
+import os
+import thulac
+
+inpath = '../law_pre/predict_data1/'
+outpath = 'dataToLabel.json'
+
+getNum = 400
+cut = thulac.thulac()
+
+
+def get(num):
+    global cut
+    fileList = os.listdir(inpath)
+    fout = open(outpath, 'a')
+    n = 0
+    for f in fileList:
+        fin = open(inpath + f, 'r')
+        line = fin.readline()
+        while line:
+            line = json.loads(line)
+            ans = []
+            if line['meta']['crit'] != 16 and line['meta']['crit'] != 19:
+                continue
+            for s in line['content']:
+                sentence = ''.join(s)
+                sentence = thulac.cut(sentence)
+                ans.append(sentence)
+            print(json.dumps(ans, ensure_ascii = False), file = fout)
+            n += 1
+            if n == num:
+                return
+
+            line = fin.readline()
+        fin.close()
+
+get(400)
